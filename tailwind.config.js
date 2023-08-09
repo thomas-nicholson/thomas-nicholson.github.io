62ec15b03debaf7fc192362fae18c01d0035806b
Thomas Nicholson <tnicholson128@protonmail.com>
2023-08-09 22:33:13 +1000

    Added tailwind styles

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.html',
+    './src/**/*.tsx', // or './src/**/*.tsx' if using TypeScript
+    // more paths where Tailwind classes may be used
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
