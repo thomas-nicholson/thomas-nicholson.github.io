81843989a28ac88f445bb1de3ba351c184fad120
tnicholson3701 <17674991-tnicholson3701@users.noreply.replit.com>
2025-01-29 08:35:48 +0000

    User checkpoint: Update Node.js version to 18 and remove Jest dependency.

--- replit.nix
+++ replit.nix
@@ -1,9 +1,7 @@
-
 { pkgs }: {
-	deps = [
-		pkgs.nodejs-20_x
-		pkgs.nodePackages.typescript-language-server
-		pkgs.yarn
-		pkgs.replitPackages.jest
-	];
+  deps = [
+    pkgs.nodejs-18_x
+    pkgs.nodePackages.typescript-language-server
+    pkgs.yarn
+  ];
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
-    allowedHosts: ['55e398dd-c42b-4c13-bfc8-ff12fa07ae0f-00-2r8193un22sci.sisko.repl.co']
+    host: "0.0.0.0",
+    allowedHosts: [
+      "55e398dd-c42b-4c13-bfc8-ff12fa07ae0f-00-2r8193un22sci.sisko.repl.co",
+    ],
   },
-})
+});
