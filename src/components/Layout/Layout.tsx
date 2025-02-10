b348f559e19dcf9cb6e6b1ee77da8c0530fa5b8f
Thomas Nicholson <tnicholson128@protonmail.com>
2025-02-10 13:04:48 +0000

    Assistant checkpoint: Add haptic feedback to mobile navigation
    Assistant generated file changes:
    - src/components/Layout/Layout.tsx: Add haptic feedback to navigation buttons
    
    ---
    
    User prompt:
    
    Great work! Now another idea, can you add haptic feedback to nav buttons?

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -24,16 +24,32 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
         </div>
         <div className="fixed bottom-0 left-0 right-0 bg-white border-t lg:hidden z-50">
           <div className="flex justify-around p-3">
-            <Link to="/" className="text-gray-700 hover:text-gray-900">
+            <Link 
+              to="/" 
+              className="text-gray-700 hover:text-gray-900"
+              onClick={() => navigator.vibrate && navigator.vibrate(50)}
+            >
               <FontAwesomeIcon icon={faHome} size="lg" />
             </Link>
-            <Link to="/projects" className="text-gray-700 hover:text-gray-900">
+            <Link 
+              to="/projects" 
+              className="text-gray-700 hover:text-gray-900"
+              onClick={() => navigator.vibrate && navigator.vibrate(50)}
+            >
               <FontAwesomeIcon icon={faLaptopCode} size="lg" />
             </Link>
-            <Link to="/blog" className="text-gray-700 hover:text-gray-900">
+            <Link 
+              to="/blog" 
+              className="text-gray-700 hover:text-gray-900"
+              onClick={() => navigator.vibrate && navigator.vibrate(50)}
+            >
               <FontAwesomeIcon icon={faBook} size="lg" />
             </Link>
-            <Link to="/contact" className="text-gray-700 hover:text-gray-900">
+            <Link 
+              to="/contact" 
+              className="text-gray-700 hover:text-gray-900"
+              onClick={() => navigator.vibrate && navigator.vibrate(50)}
+            >
               <FontAwesomeIcon icon={faEnvelope} size="lg" />
             </Link>
           </div>
