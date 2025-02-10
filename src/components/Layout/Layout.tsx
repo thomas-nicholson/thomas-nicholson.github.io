6c54219eb0aeb9a3ed966f20908d9ac9bcf4484a
Thomas Nicholson <tnicholson128@protonmail.com>
2025-02-10 13:00:57 +0000

    Assistant checkpoint: Add icon-based mobile navigation
    Assistant generated file changes:
    - src/components/Layout/Layout.tsx: Update imports to include new icons, Replace hamburger menu with icon navigation
    
    ---
    
    User prompt:
    
    When the site is in mobile format, instead of using a hamburger menu that drops down, could we switch it to use icons that represent what the pages are, for example, a house for home, perhaps a Web browser icon or something similar for projects, a document and pencil for blog, and an SMS bubble or email icon for contact. These are just examples, I want you to come up with what you think best represents these pages

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faLaptopCode, faBook, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 
 interface LayoutProps {
@@ -14,17 +14,28 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="container mx-auto text-black bg-white">
       <nav className="flex flex-wrap justify-between p-4">
-        <div className="flex items-center">
-        <button onClick={() => setMenuOpen(!menuOpen)} className="lg:hidden px-2 py-1">
-          <FontAwesomeIcon icon={faBars} />
-        </button>
-        </div>
-        <div className={`w-full lg:flex lg:items-center lg:w-auto ${menuOpen ? '' : 'hidden lg:flex'}`}>
+        <div className="hidden lg:flex lg:items-center lg:w-auto">
           <div className="text-sm lg:flex-grow">
-            <Link to="/" className="block mt-4 lg:inline-block lg:mt-0 px-4 py-2 hover:underline">Home</Link>
-            <Link to="/projects" className="block mt-4 lg:inline-block lg:mt-0 px-4 py-2 hover:underline">Projects</Link>
-            <Link to="/blog" className="block mt-4 lg:inline-block lg:mt-0 px-4 py-2 hover:underline">Blog</Link>
-            <Link to="/contact" className="block mt-4 lg:inline-block lg:mt-0 px-4 py-2 hover:underline">Contact</Link>
+            <Link to="/" className="lg:inline-block lg:mt-0 px-4 py-2 hover:underline">Home</Link>
+            <Link to="/projects" className="lg:inline-block lg:mt-0 px-4 py-2 hover:underline">Projects</Link>
+            <Link to="/blog" className="lg:inline-block lg:mt-0 px-4 py-2 hover:underline">Blog</Link>
+            <Link to="/contact" className="lg:inline-block lg:mt-0 px-4 py-2 hover:underline">Contact</Link>
+          </div>
+        </div>
+        <div className="fixed bottom-0 left-0 right-0 bg-white border-t lg:hidden">
+          <div className="flex justify-around p-3">
+            <Link to="/" className="text-gray-700 hover:text-gray-900">
+              <FontAwesomeIcon icon={faHome} size="lg" />
+            </Link>
+            <Link to="/projects" className="text-gray-700 hover:text-gray-900">
+              <FontAwesomeIcon icon={faLaptopCode} size="lg" />
+            </Link>
+            <Link to="/blog" className="text-gray-700 hover:text-gray-900">
+              <FontAwesomeIcon icon={faBook} size="lg" />
+            </Link>
+            <Link to="/contact" className="text-gray-700 hover:text-gray-900">
+              <FontAwesomeIcon icon={faEnvelope} size="lg" />
+            </Link>
           </div>
         </div>
       </nav>
