6a22faa5c09d9755a10f5a75bb4c9b67cdf6bdb4
Thomas Nicholson <tnicholson128@protonmail.com>
2025-07-03 14:05:16 +1000

    fix: remove extra closing tag causing ts compile error

--- src/components/Pages/Contact.tsx
+++ src/components/Pages/Contact.tsx
@@ -100,7 +100,6 @@ const Contact: React.FC = () => {
               </div>
               <button type="submit" className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">Send</button>
             </form>
-          </div>
         </div>
       </div>
     </div>
