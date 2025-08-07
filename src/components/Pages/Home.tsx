a706c425f48036b4c562e37381a7d20f7efe468d
Thomas Nicholson <tnicholson128@protonmail.com>
2025-08-07 06:00:58 +0000

    merge changes

--- src/components/Pages/Home.tsx
+++ src/components/Pages/Home.tsx
@@ -5,7 +5,7 @@ const Home: React.FC = () => (
     <div className="max-w-4xl mx-auto p-8">
       <div className="text-center mb-16">
         <h1 className="text-5xl font-bold text-gray-800 dark:text-gray-100 mb-4">
-          Thomas Nicholson
+          Thomas
         </h1>
         <p className="text-xl text-gray-600 dark:text-gray-300">Full-Stack Software Engineer</p>
       </div>
